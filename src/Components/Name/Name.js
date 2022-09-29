82fabb67b5fd6438bb25e3b13aac3239d0a1827c
masniloy <masaimunniloy88@gmail.com>
2022-09-29 13:30:17 +0600

    logo done

---
+++ src/Components/Name/Name.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Name.css'
+
+const name = () => {
+    return (
+        <div className='name'>
+            <img id='logo' src="favicon 2.png" alt="" />
+            <h1 id='lname'>POWER HOUSE</h1>
+        </div>
+    );
+};
+
+export default name;
